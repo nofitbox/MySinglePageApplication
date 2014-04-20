a3b79afba10b79ef4c1b44a8e0429e50546ce10b
nofitbox <nofitbox@gmail.com>
2014-04-20 16:26:42 -0400

    Initial commit

---
+++ MvcApplication/Models/Entity.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using Newtonsoft.Json;
+
+namespace MvcApplication.Models
+{
+    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
+    public abstract class Entity
+    {
+        [JsonProperty(PropertyName = "id")]
+        [JsonConverter(typeof(ObjectIdConverter))]
+        public ObjectId Id { get; protected set; }
+    }
+}
